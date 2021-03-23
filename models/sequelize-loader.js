66027517e2879d8e5476a0202d5199fd54c6819c
matsukawa-s <syoudai.hand@gmail.com>
2021-03-24 00:31:38 +0900

    SSLの設定

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,12 @@
 "use strict";
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "postgres://postgres:postgres@localhost/todo"
+  process.env.DATABASE_URL || "postgres://postgres:postgres@localhost/todo",
+  {
+    dialectOptions: {
+      ssl: true,
+    },
+  }
 );
 
 module.exports = {
